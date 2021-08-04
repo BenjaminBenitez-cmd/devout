02ae6a12596441bcf017b39c2e0999abc4e43493
BenjaminBenitez-cmd <benitezbenjamin853@gmail.com>
2021-08-04 11:13:01 -0600

    added ssl option to db

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,6 +8,7 @@ const connectionString = `postgresql://${config.PGUSER}:${config.PGPASSWORD}@${c
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: { rejectUnauthorized: false },
 });
 
 const db = {
