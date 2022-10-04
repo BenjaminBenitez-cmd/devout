2291561cd0f592700510bd94501d743943e6fc84
BenjaminBenitez-cmd <benitezbenjamin853@gmail.com>
2022-10-04 17:34:00 -0600

    added new fonts

--- client/src/components/footers/Store.js
+++ client/src/components/footers/Store.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row } from "reactstrap";
 import FeatherIcons from "feather-icons-react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -9,25 +10,32 @@ const Footer = () => {
         <p>&copy; Devout</p>
       </Col>
       <Col xs={6} sm={3} md={3} lg={1}>
-        <p className="text-upper">Newsletter</p>
+        <Link to="template">
+          <p className="text-upper">Newsletter</p>
+        </Link>
       </Col>
       <Col xs={6} sm={3} md={3} lg={1}>
-        <p className="text-upper">Returns</p>
+        <Link to="template">
+          <p className="text-upper">Returns</p>
+        </Link>
       </Col>
       <Col xs={6} sm={3} md={3} lg={2}>
-        <p className="text-upper">Sneaker news</p>
+        <Link to="template">
+          <p className="text-upper">Sneaker news</p>
+        </Link>
       </Col>
       <Col xs={6} sm={3} md={3} lg={1}>
-        <p className="text-upper">FAQ</p>
+        <Link to="template">
+          <p className="text-upper">FAQ</p>
+        </Link>
       </Col>
       <Col xs={6} sm={3} md={3} lg={2}>
         <p className="text-upper">Privacy Policy</p>
       </Col>
       <Col xs={6} sm={3} md={3} lg={{ size: 1, offset: 2 }}>
-        <FeatherIcons icon="facebook" />
-      </Col>
-      <Col xs={6} sm={3} md={3} lg={1}>
-        <FeatherIcons icon="instagram" />
+        <a href="https://github.com/BenjaminBenitez-cmd/devout">
+          <FeatherIcons icon="github" />
+        </a>
       </Col>
     </Row>
   );
